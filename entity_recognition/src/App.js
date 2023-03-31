ee9f18c02501ba559557cd0b1d39b588b546ef13
linjia-long <linjial2@illinois.edu>
2023-03-31 00:24:38 -0500

    clean code - merge frongtend and backend work for the last week

--- entity_recognition/src/App.js
+++ entity_recognition/src/App.js
@@ -20,8 +20,8 @@ function App() {
     const response = await axios.get(`http://127.0.0.1:8000/wiki/get_ner_on/?topic="${topic}"`, {
         withCredentials: false,
       });
-      const output = response.data.entity;
-      const output_format = output.replace(/\n/g, "<br />");
+    const output = response.data.entity;
+    const output_format = output.replace(/\n/g, "<br />");
     setResult(output_format)
   }
 
@@ -45,33 +45,7 @@ function fileClickHandler(e) {
   }
   
 
-  /* The state for WikiBlock. 
-   * resultWiki is a string containing NER output.
-   * setResultWiki acts as a function that changes resultWiki, with rerendering.
-   */
-  let [resultWiki, setResultWiki] = useState("show results here")
 
-
-  /* handleSubmitWiki is the event handler for the WikiBlock. 
-   * It receives a wiki "topic" and calls the corresponding web address,
-   * which triggers django backend to run NER algorithm and send back json info,
-   * which we then use to update our resultWiki state.
-   */
-  const handleSubmitWiki = async (topic) => {
-    const response = await axios.get(`http://127.0.0.1:8000/wiki/get_ner_on/?topic="${topic}"`, {
-      withCredentials: false,
-    });
-    const output = response.data.entity;
-    const output_format = output.replace(/\n/g, "<br />");
-    setResult(output);
-  }
-
-
-  /* We are currently focusing on 3 primary blocks: TextBlock, FileBlock, WikiBlock 
-   * TextBlock receives NER output from user input text.
-   * TextBlock receives NER output from user input file.
-   * WikiBlock receives NER output from user specified wikipedia topic.
-   */
   return (
     <>
     <div>
